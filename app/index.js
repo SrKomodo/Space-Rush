39827697c29f787ba04ea2330c1b8d632555e941
SrKomodo <komodosr@gmail.com>
2017-07-14 18:37:24 -0300

    Set up states

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-let game = new Phaser.Game()
+let titleState = {
+
+};
+
+let playState = {
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  let game = new Phaser.Game(640, 480, Phaser.AUTO, "game", null, false, false);
+  game.state.add("title", titleState, false);
+  game.state.add("play", playState, false);
+
+  game.state.start("title");
+});
